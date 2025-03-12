c174ffa0eddead6879bc88ad1a9bbc21530c39a9
yelsinkiprop <yelsin.kiprop@student.moringaschool.com>
2025-03-12 23:36:33 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 16;
+let num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 4, 20);
